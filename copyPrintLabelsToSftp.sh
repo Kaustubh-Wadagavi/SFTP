8248015a7edec12413563159f07ead71e4fb4cdd
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-05-13 13:19:46 +0530

    Created main and error function.

--- copyPrintLabelsToSftp.sh
+++ copyPrintLabelsToSftp.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-CONFIG_FILE=$1
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 
 sendEmail() {
-  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $EMAIL_ID:$EMAIL_PASS --mail-from $EMAIL_ID --mail-rcpt $RCPT_EMAIL_ID --upload-file $EMAIL_LOG_FILE_PATH/$current_time.log
-
+  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $EMAIL_ID:$EMAIL_PASS --mail-from $EMAIL_ID --mail-rcpt $RCPT_EMAIL_ID --upload-file $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log
 }
 
 moveFiles() {
@@ -20,10 +18,6 @@ moveFiles() {
       mv "${SUB_DIR}"/* "${MOVING_FILES_LOCATION}${DIR}"
     fi
   done
-   
-  rm -f $TEMP_FILE
-  rm -f $LOCK_FILE
-  exit 0;
 
 }
 
@@ -35,23 +29,16 @@ transferFiles() {
     expect \"password: \"
     send \"${PASSWORD}\r\"
     expect eof
-  " >> $EMAIL_LOG_FILE_PATH/$current_time.log
-  
+  " >> $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log
+
   SFTP_EXIT_CODE=$?
-  echo "SFTP EXIT CODE IS: $SFTP_EXIT_CODE" >> $EMAIL_LOG_FILE_PATH/$current_time.log
-  
-  if [[ $SFTP_EXIT_CODE -eq 0 ]]
-  then
-    echo "Files are uploaded successfully" >> $EMAIL_LOG_FILE_PATH/$current_time.log
-    moveFiles
-  else
-    sendEmail
-  fi
+  echo "SFTP EXIT CODE IS: $SFTP_EXIT_CODE" >> $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log
+  return $SFTP_EXIT_CODE
 
 }
 
-startLog() {
-  cat > $EMAIL_LOG_FILE_PATH/$current_time.log << EOF
+enableLogging() {
+  cat > $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log << EOF
 Subject: [ IMPORTANT ALERT ] : '$CLIENT_NAME_AND_ENVIRONMENT' : SFTP COPY SCRIPT FAILED!
         
 Hello Build Team,
@@ -62,12 +49,11 @@ Please check the below errors
 
 EOF
 
-  echo "$(date +%F-%T)-INFO- Synchronizing: Found files in local folder to upload." >> $EMAIL_LOG_FILE_PATH/$current_time.log
-  transferFiles
-
+  echo "$(date +%F-%T)-INFO- Synchronizing: Found files in local folder to upload." >> $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log
+  
 }
 
-createSftpBatchFile() {
+generateTransferBatchFile() {
   trap "rm -f $TEMP_FILE" 0 1 15
   trap "rm -f $LOCK_FILE; exit" INT TERM EXIT
   echo $$ > $LOCK_FILE
@@ -81,23 +67,43 @@ createSftpBatchFile() {
     done
   done
 
+}
+
+main() {
+  CONFIG_FILE=$1
+
+  if [ ! -f "$CONFIG_FILE" ]
+  then
+    echo "Please input the config file"
+    exit 0;
+  fi
+
+  source $CONFIG_FILE
+  generateTransferBatchFile
+
   if [ -s "$TEMP_FILE" ]
-  then	    
-    startLog
+  then
+    enableLogging
   else
-    echo "No files found to transfer" >> $EMAIL_LOG_FILE_PATH/$current_time.log
+    echo "No files found to transfer" >> $EMAIL_LOG_FILE_PATH/$CURRENT_TIME.log
     rm -f $TEMP_FILE
     rm -f $LOCK_FILE
     exit 0;
   fi
 
+  transferFiles
+  local STATUS=$?
+
+  if [ $STATUS -eq 0 ]
+  then
+    moveFiles
+    rm -f $TEMP_FILE
+    rm -f $LOCK_FILE
+    exit 0;
+  else
+    sendEmail
+  fi
+
 }
-
-if [ ! -f "$CONFIG_FILE" ]
-then
-  echo "Please input the config file"
-  exit 0;
-fi
-
-source $CONFIG_FILE
-createSftpBatchFile 
+   
+main;
